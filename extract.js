1f83ae106689c28ef4d44b768b53c12eec857326
Sho <sfaizi1@outlook.com>
2017-01-23 15:35:18 -0800

    create and use custom module

---
+++ extract.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+var extractFilePath = function (url) {
+  var filePath;
+  var fileName = 'index.html';
+
+  if (url.length > 1) {
+    fileName = url.substring(1);
+  // to strip off the 1st character which will be '/'
+  }
+  console.log('The file is: ' + fileName);
+
+  filePath = path.resolve(__dirname, 'app', fileName);
+  return filePath;
+}
+
+module.exports = extractFilePath;
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var http = require('http');
 // access HTTP module
 var fs = require('fs');
+var extract = require('./extract')
 
 var server = http.createServer(function (req,res) {
   console.log('Responding to a request');
-  var url = req.url;
 
-  var fileName = 'index.html';
-  if (url.length > 1) {
-    fileName = url.substring(1);
-    // to strip off the 1st character which will be '/'
-  }
-  console.log(fileName);
-  fs.readFile('app/index.html', function (err, data) {
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function (err, data) {
     res.end(data);
   })
 });
