624702b004b9bf1110d75e3929a24d10b98f995e
Sho <sfaizi1@outlook.com>
2017-01-23 20:29:01 -0800

    add MIME type

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var http = require('http');
 // access HTTP module
 var fs = require('fs');
-var extract = require('./extract')
+var extract = require('./extract');
+
+var handleError = function (err, res) {
+  res.writeHead(404);
+  res.end();
+};
 
 var server = http.createServer(function (req,res) {
   console.log('Responding to a request');
-
   var filePath = extract(req.url);
   fs.readFile(filePath, function (err, data) {
-    res.end(data);
-  })
+    if(err) {
+      handleError(err,res);
+      return;
+    }else{
+      res.setHeader('Content-Type', 'text/html');
+      // setting up MIME type
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000);
