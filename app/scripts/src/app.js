07a903083b0e7a25dea41eefb4b60838bd0c19d5
Sho <sfaizi1@outlook.com>
2017-01-25 13:05:01 -0800

    prompt user for username

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,15 +1,18 @@
 // this module will define the structure of messages and pass messages between ws-client and dom
 import socket from './ws-client';
-import {ChatForm, ChatList} from './dom';
+import {ChatForm, ChatList, promptForUsername} from './dom';
 
 const FORM_SELECTOR = '[data-chat="chat-form"]';
 const INPUT_SELECTOR = '[data-chat="message-input"]';
 const LIST_SELECTOR = '[data-chat="message-list"]';
 
+let username = '';
+username = promptForUsername();
+
 class ChatApp {
   constructor() {
     this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
-    this.chatList = new ChatList(LIST_SELECTOR, 'Superman');
+    this.chatList = new ChatList(LIST_SELECTOR, username);
 
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
@@ -33,7 +36,7 @@ class ChatApp {
 class ChatMessage {
   constructor({
     message: m,
-    user: u='Batman',
+    user: u = username,
     timestamp: t=(new Date()).getTime()
   }) {
     this.message = m;
