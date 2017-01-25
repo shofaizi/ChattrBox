23e87d7ecc47a758a5c60da5af5b49b5e3e9bd3e
Sho <sfaizi1@outlook.com>
2017-01-25 12:50:46 -0800

    create chatlist class contd

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,13 +1,15 @@
 // this module will define the structure of messages and pass messages between ws-client and dom
 import socket from './ws-client';
-import {ChatForm} from './dom';
+import {ChatForm, ChatList} from './dom';
 
 const FORM_SELECTOR = '[data-chat="chat-form"]';
 const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
 
 class ChatApp {
   constructor() {
-    this.chatForm = new ChatForm(FORM_SELECTOR,INPUT_SELECTOR);
+    this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+    this.chatList = new ChatList(LIST_SELECTOR, 'Superman');
 
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
@@ -16,11 +18,14 @@ class ChatApp {
         socket.sendMessage(message.serialize());
       // when user submits a message in the form the ChatForm instance will take
       // that data and send it to ChatApp's callback, and the callback will then
-      // package it up as a ChatMessage and send it to the WebSocket server 
+      // package it up as a ChatMessage and send it to the WebSocket server
       });
     });
     socket.registerMessageHandler((data) => {
       console.log(data);
+      let message = new ChatMessage(data);
+      this.chatList.drawMessage(message.serialize());
+      // draw new messages as they come in by calling chatList.drawMessage
     });
   }
 }
@@ -28,7 +33,7 @@ class ChatApp {
 class ChatMessage {
   constructor({
     message: m,
-    user: u='batman',
+    user: u='Batman',
     timestamp: t=(new Date()).getTime()
   }) {
     this.message = m;
--- app/scripts/src/dom.js
+++ app/scripts/src/dom.js
@@ -2,12 +2,12 @@ import $ from 'jquery'; //no need to specify path since this is now a library
 
 export class ChatForm {
   constructor(formSel, inputSel) {
-    this.$form = formSel;
-    this.$input = inputSel;
+    this.$form = $(formSel);
+    this.$input = $(inputSel);
   }
 
   init(submitCallback) {
-    this.$form.submit((event) {
+    this.$form.submit((event) => {
       event.preventDefault();
       let val = this.$input.val();
       // grab value and assign to variable
@@ -20,3 +20,42 @@ export class ChatForm {
   // omit curly braces when returning the result of a singe statement👆
   }
 }
+
+export class ChatList {
+  constructor(listSel, username) {
+    this.$list = $(listSel);
+    this.username = username;
+  }
+
+  drawMessage({user: u, timestamp: t, message: m}) {
+    let $messageRow = $('<li>', {
+      'class': 'message-row'
+    });
+
+    if (this.username === u) {
+      $messageRow.addClass('me');
+    }
+
+    let $message = $('<p>');
+
+    $message.append($('<span>', {
+      'class': 'message-username',
+      text: u
+    }));
+
+    $message.append($('<span>', {
+      'class': 'timestamp',
+      'data-time': t,
+      text: (new Date(t)).getTime()
+    }));
+
+    $message.append($('<span>', {
+      'class': 'message-message',
+      text: m
+    }));
+
+    $messageRow.append($message);
+    $(this.listId).append($messageRow);
+    $messageRow.get(0).scrollIntoView();
+  }
+};
