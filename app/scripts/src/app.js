606a2db8612cd26e0f2c786fc4b309b82057a442
Sho <sfaizi1@outlook.com>
2017-01-25 13:12:00 -0800

    user session storage

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,13 +1,18 @@
 // this module will define the structure of messages and pass messages between ws-client and dom
 import socket from './ws-client';
 import {ChatForm, ChatList, promptForUsername} from './dom';
+import {UserStore} from './storage';
 
 const FORM_SELECTOR = '[data-chat="chat-form"]';
 const INPUT_SELECTOR = '[data-chat="message-input"]';
 const LIST_SELECTOR = '[data-chat="message-list"]';
 
-let username = '';
-username = promptForUsername();
+let userStore = new UserStore('x-chattrbox/u');
+let username = userStore.get();
+if(!username) {
+  username = promptForUsername();
+  userStore.set(username);
+}
 
 class ChatApp {
   constructor() {
